bb0cea55041437be42afbe9cc460099d9f6cf753
Lucas Saraiva <lucas.dsra@gmail.com>
2025-04-22 11:05:32 -0300

    Criação do select com group by (operadoras fixas e de celular)

--- app/controllers/listaTelefonica.js
+++ app/controllers/listaTelefonica.js
@@ -1,16 +1,30 @@
 angular.module('listaTelefonica').controller('listaTelefonicaCtrl', function($scope){
  $scope.name = "Lista Telefonica"
+ 
     $scope.contatos = [
         {name: "Lucas", telefone:  "9489191986"},
         {name: "Natan", telefone:  "94875691986"},
         {name: "Emerson", telefone:  "9487561986"},
         {name: "Gabigol", telefone:  "947561986"},
+    ];
+    $scope.operadoras = [
+        {name : "oi", codigo: 14, categoria: "Celular"},
+        {name : "Tim", codigo: 41,  categoria: "Celular"},
+        {name : "Vivo", codigo: 15,  categoria: "Celular"},
+        {name : "Claro", codigo: 21, categoria: "Celular"},
+        {name : "Assantet", codigo: 51, categoria: "Fixo"},
+
+
     ]
+
     $scope.adicionarContatos = function(contato){
+
         if(contato === undefined){
             alert("Necessário Prenncher os Campos")
             return;
-        }
+        };
+
+        
         console.log($scope.contato)
         $scope.contatos.push(angular.copy(contato))
         delete $scope.contato;
